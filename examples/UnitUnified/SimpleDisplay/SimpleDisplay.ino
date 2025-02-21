e44a2f5210cfa6346dcb83e34e630b4f45c8a95d
GOB <goblin52@gmail.com>
2025-02-21 16:01:08 +0900

    Add examples

---
+++ examples/UnitUnified/SimpleDisplay/SimpleDisplay.ino
@@ -0,0 +1,19 @@
+/*
+ * SPDX-FileCopyrightText: 2025 M5Stack Technology CO LTD
+ *
+ * SPDX-License-Identifier: MIT
+ */
+/*
+  Display example using M5UnitUnified for UnitCardKB/UnitFacesQWERTY
+*/
+// *************************************************************
+// Choose one define symbol to match the unit you are using
+// *************************************************************
+#if !defined(USING_UNIT_CARDKB) && !defined(USING_UNIT_FACES_QWERTY)
+// For CardKB
+// #define USING_UNIT_CARDKB
+// For FacesQWERTY
+// #define USING_UNIT_FACES_QWERTY
+#endif
+// *************************************************************
+#include "main/SimpleDisplay.cpp"
--- examples/UnitUnified/UnitCardKB/PlotToSerial/PlotToSerial.ino
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-FileCopyrightText: 2025 M5Stack Technology CO LTD
- *
- * SPDX-License-Identifier: MIT
- */
-/*
-  Example using M5UnitUnified for UnitCardKB
-*/
-#include "main/PlotToSerial.cpp"
